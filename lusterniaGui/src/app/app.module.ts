0b0230c1bf7dd1ba276e73dbea6783780eb11f5c
Richard Kille <richard.kille@pushdr.com>
2019-12-05 16:45:23 +0000

    initial commit for angular

---
+++ lusterniaGui/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {Route, RouterModule} from '@angular/router';
+import { WrapperComponent } from './wrapper/wrapper.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: WrapperComponent,
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WrapperComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
